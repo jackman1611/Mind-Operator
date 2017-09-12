b873083c14d055f0dab3da44521047b87f0be520
JACK <JACK@GAIA>
2017-09-12 16:42:02 -0500

    Funciones de las operaciones programadas incluye el TIMER

--- src/Default/Principal.java
+++ src/Default/Principal.java
@@ -1,8 +1,11 @@
 package Default;
 
-public class Principal extends javax.swing.JFrame {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-   
+public class Principal extends javax.swing.JFrame {
+int a=0,b=0,c=0,sec=0;
+
     public Principal() {
         initComponents();
     }
@@ -13,17 +16,16 @@ public class Principal extends javax.swing.JFrame {
 
         jTabbedPane2 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
-        A = new javax.swing.JLabel();
-        B = new javax.swing.JLabel();
         SIMB = new javax.swing.JLabel();
         SIMB1 = new javax.swing.JLabel();
-        C = new javax.swing.JLabel();
         Resp = new javax.swing.JTextField();
         B1 = new javax.swing.JLabel();
-        siguente = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        comenzar = new javax.swing.JButton();
-        time = new javax.swing.JLabel();
+        NEW = new javax.swing.JButton();
+        BEGIN = new javax.swing.JButton();
+        C = new javax.swing.JTextField();
+        A = new javax.swing.JTextField();
+        B = new javax.swing.JTextField();
+        TIME = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
@@ -35,16 +37,6 @@ public class Principal extends javax.swing.JFrame {
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        A.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
-        A.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        A.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel1.add(A, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, 90, 70));
-
-        B.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
-        B.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        B.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel1.add(B, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 30, 90, 70));
-
         SIMB.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
         SIMB.setText("=");
         jPanel1.add(SIMB, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 30, -1, 70));
@@ -52,11 +44,6 @@ public class Principal extends javax.swing.JFrame {
         SIMB1.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
         SIMB1.setText("+");
         jPanel1.add(SIMB1, new org.netbeans.lib.awtextra.AbsoluteConstraints(120, 30, -1, 70));
-
-        C.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
-        C.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        C.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel1.add(C, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 30, 110, 70));
 
         Resp.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
         Resp.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -69,22 +56,45 @@ public class Principal extends javax.swing.JFrame {
         B1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
         jPanel1.add(B1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 150, 260, 70));
 
-        siguente.setText("SIGUIENTE");
-        jPanel1.add(siguente, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 250, 90, -1));
-
-        jButton2.setText("NUEVO");
-        jPanel1.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, 90, -1));
-
-        comenzar.setText("COMENZAR");
-        comenzar.addActionListener(new java.awt.event.ActionListener() {
+        NEW.setText("NUEVO");
+        NEW.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                comenzarActionPerformed(evt);
+                NEWActionPerformed(evt);
             }
         });
-        jPanel1.add(comenzar, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 250, -1, -1));
+        jPanel1.add(NEW, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 290, 90, -1));
 
-        time.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "TIEMPO", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Century Gothic", 1, 18))); // NOI18N
-        jPanel1.add(time, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 60, 130, 120));
+        BEGIN.setText("COMENZAR");
+        BEGIN.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BEGINActionPerformed(evt);
+            }
+        });
+        jPanel1.add(BEGIN, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 250, -1, -1));
+
+        C.setEditable(false);
+        C.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
+        C.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        C.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel1.add(C, new org.netbeans.lib.awtextra.AbsoluteConstraints(300, 30, 90, 70));
+
+        A.setEditable(false);
+        A.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
+        A.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        A.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel1.add(A, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 30, 90, 70));
+
+        B.setEditable(false);
+        B.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
+        B.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        B.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        jPanel1.add(B, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 30, 90, 70));
+
+        TIME.setEditable(false);
+        TIME.setFont(new java.awt.Font("Century Gothic", 3, 48)); // NOI18N
+        TIME.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        TIME.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "TIEMPO", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Century Gothic", 3, 18))); // NOI18N
+        jPanel1.add(TIME, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 20, 130, 100));
 
         jTabbedPane2.addTab("SUMA", jPanel1);
 
@@ -112,7 +122,7 @@ public class Principal extends javax.swing.JFrame {
             .addGap(0, 332, Short.MAX_VALUE)
         );
 
-        jTabbedPane2.addTab("DIVISION", jPanel3);
+        jTabbedPane2.addTab("MULTIPLICACION", jPanel3);
 
         getContentPane().add(jTabbedPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 580, 360));
 
@@ -129,14 +139,59 @@ public class Principal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void comenzarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_comenzarActionPerformed
+    private void BEGINActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BEGINActionPerformed
+    
+    try {
+        generar();
+        suma();
         
-    }//GEN-LAST:event_comenzarActionPerformed
+        tiempo();
+    } catch (InterruptedException ex) {
+        Logger.getLogger(Principal.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  
+    }//GEN-LAST:event_BEGINActionPerformed
 
+    private void NEWActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_NEWActionPerformed
+    
+    }//GEN-LAST:event_NEWActionPerformed
+
+    public void generar() {
+    sec=15;
+    a = (int) (Math.random() * 99) + 1;
+    b = (int) (Math.random() * 99) + 1;
+    A.setText(String.valueOf(a));
+    B.setText(String.valueOf(b));
+    
+    }
+    public void suma() {
+    c=a+b;
+     }
+    public void resta() {
+    c=a-b;
+     }
+    public void multiplicacion() {
+    c=a*b;
+     }
+    public void tiempo() throws InterruptedException{
+              try{ 
+            while (true) {            
+            Thread.sleep(1000);
+            TIME.setText(String.valueOf(sec));
+            sec--;
+            if (sec==0) {
+            C.setText(String.valueOf(c));
+            TIME.setText(String.valueOf(sec));
+            break;
+            }}
+        } catch(InterruptedException e ) { 
+            }
+    }
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[])throws InterruptedException {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -160,6 +215,7 @@ public class Principal extends javax.swing.JFrame {
         }
         //</editor-fold>
 
+        
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
@@ -169,15 +225,16 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel A;
-    private javax.swing.JLabel B;
+    private javax.swing.JTextField A;
+    private javax.swing.JTextField B;
     private javax.swing.JLabel B1;
-    private javax.swing.JLabel C;
+    private javax.swing.JButton BEGIN;
+    private javax.swing.JTextField C;
+    private javax.swing.JButton NEW;
     private javax.swing.JTextField Resp;
     private javax.swing.JLabel SIMB;
     private javax.swing.JLabel SIMB1;
-    private javax.swing.JButton comenzar;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTextField TIME;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
@@ -185,7 +242,7 @@ public class Principal extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JTabbedPane jTabbedPane2;
-    private javax.swing.JButton siguente;
-    private javax.swing.JLabel time;
     // End of variables declaration//GEN-END:variables
+
+
 }
